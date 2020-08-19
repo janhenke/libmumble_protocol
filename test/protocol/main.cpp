3470705e783f9c2f53e05f0ca8646872b1d5b201
Jan Henke <Jan.Henke@taujhe.de>
2020-08-19 23:42:46 +0200

    Add unit testing

---
+++ test/protocol/main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by jan on 19.08.20.
+//
+#define BOOST_TEST_MODULE protocol
+
+#include <boost/test/unit_test.hpp>
---
+++ test/protocol/voice.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jan on 19.08.20.
+//
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test) {
+	BOOST_CHECK(true);
+}
