9c58b6b12690f33515c80522141194beaea11edd
Jan Henke <Jan.Henke@taujhe.de>
2020-09-19 22:01:27 +0200

    Rework the CMake project

--- src/temp.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by jan on 16.09.20.
-//
-
-void dummy_export_function() {
-	return;
-}
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,6 @@
 //
 // Created by jan on 19.08.20.
 //
-#define BOOST_TEST_MODULE libmumble_client_protocol
+#define BOOST_TEST_MODULE libmumble_client_test
 
 #include <boost/test/unit_test.hpp>
