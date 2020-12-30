d8dc38cbd40c1a161c53f37a2cce820461a18f47
Jan Henke <Jan.Henke@taujhe.de>
2020-12-30 21:39:33 +0100

    Move Dockerfile to separate repository

--- build-container/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-docker build -t docker-hosted.taujhe.de/build/libmumble_client build-container
-
-docker push docker-hosted.taujhe.de/build/libmumble_client
-
-docker rmi docker-hosted.taujhe.de/build/libmumble_client
